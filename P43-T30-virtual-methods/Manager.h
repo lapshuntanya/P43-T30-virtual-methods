9b7b0cefa01538e44f48b08c31007c613ac7785b
lapshun <lapshun.tatiana@gmail.com>
2025-10-15 18:07:05 +0300

    Manager

---
+++ P43-T30-virtual-methods/Manager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Employee.h"
+class Manager :
+    public Employee
+{
+protected:
+    int percent;
+    float sales;
+public:
+    Manager();
+    Manager(string name, float salary, int percent, float sales);
+
+    void setPercent(int percent);
+    void setSales(float sales);
+
+    int getPercent()const;
+    float getSales()const;
+
+    void showInfo()const;
+    float calcSalary()const;
+};
+
