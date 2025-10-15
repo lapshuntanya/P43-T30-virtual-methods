7bc1f65903369eae3548d69a66c71e15a10f63ca
lapshun <lapshun.tatiana@gmail.com>
2025-10-15 17:36:47 +0300

    Employee

---
+++ P43-T30-virtual-methods/Employee.cpp
@@ -0,0 +1,29 @@
+#include "Employee.h"
+
+Employee::Employee()
+{
+    name = "undefined";
+    salary = 10'000;
+}
+
+Employee::Employee(string name, float salary)
+{
+    this->name = name;
+    this->salary = salary;
+}
+
+void Employee::setName(string name){    this->name = name;}
+void Employee::setSalary(float salary){    this->salary = salary;}
+
+string Employee::getName() const{   return name;}
+float Employee::getSalary() const{    return salary;}
+
+void Employee::showInfo() const{
+    cout << "\t Employee:\n";
+    cout << "Name: " << name << endl;
+    cout << "Salary: " << salary << "UAH\n";
+}
+
+float Employee::calcSalary() const{
+    return salary - salary*0.22;
+}
