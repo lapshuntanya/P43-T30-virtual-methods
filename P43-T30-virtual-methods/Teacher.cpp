37912bc87c8e9534e286f86734c9997957bc625e
lapshun <lapshun.tatiana@gmail.com>
2025-10-15 17:51:32 +0300

    Teacher

--- P43-T30-virtual-methods/Teacher.cpp
+++ P43-T30-virtual-methods/Teacher.cpp
@@ -2,35 +2,32 @@
 
 Teacher::Teacher()
 {
+    cost = 0;
+    hours = 0;
 }
 
 Teacher::Teacher(string name, float salary, float cost, int hours)
+    :Employee(name, salary)
 {
+    this->cost = cost;
+    this->hours = hours;
 }
 
-void Teacher::setCost(float cost)
-{
-}
+void Teacher::setCost(float cost){    this->cost = cost;}
+void Teacher::setHours(int hours){    this->hours = hours;}
 
-void Teacher::setHours(int hours)
-{
-}
-
-float Teacher::getCost() const
-{
-    return 0.0f;
-}
-
-int Teacher::getHours() const
-{
-    return 0;
-}
+float Teacher::getCost() const{   return cost;}
+int Teacher::getHours() const{    return hours;}
 
 void Teacher::showInfo() const
 {
+    cout << "++++++++++++++++++++++++++\n";
+    cout << "TEACHER ";
+    Employee::showInfo();
+    cout << "Hours: " << hours << endl;
+    cout << "Cost per 1h: " << cost << "UAH\n";
 }
 
-float Teacher::calcSalary() const
-{
-    return 0.0f;
+float Teacher::calcSalary() const{
+    return (salary + hours*cost) *0.78;
 }
